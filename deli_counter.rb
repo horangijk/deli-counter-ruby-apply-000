307f6d110801cd6e557f9f006c7f60abd5a44d47
Jerold Kim <iamjooyoung@gmail.com>
2018-11-07 18:11:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,16 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+  i = 0 
+def take_a_number_alt(katz_deli)
+  i += 1
+  katz_deli << i
+  return i
+end
 
+def reset_i
+  i = 0 
+end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
